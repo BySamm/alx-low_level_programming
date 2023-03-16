a19e06d6c2d577900c45fed88d8c332cd2b3fb97
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-17 01:47:46 +0200

    Printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - executor function
+ * Description: The program uses puts to print
+ * Return: 0
+*/
+int main(void)
+{
+char put = "\"Programming is like building a multilingual puzzle";
+puts(put);
+return (0);
+}
