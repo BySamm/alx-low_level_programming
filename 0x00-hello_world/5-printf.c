0fa4afbaa6b44ec423d2bdc7850d3d83287baf16
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-17 01:57:15 +0200

    Using printif

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - executor
+ * Description: The use of printf
+ * Return: 0
+ */
+
+int main(void)
+{
+char print = "with proper grammar, but the outcome is a piece of art,";
+printf(print);
+return (0);
+}
