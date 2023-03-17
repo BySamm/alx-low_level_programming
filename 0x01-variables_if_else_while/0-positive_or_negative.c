7d92f197522831dfd0c12b6ab044130898681ba2
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-17 14:59:58 +0200

    Checking for + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	/* Chech if the scanned number */
 	if (n > 0)
 	{
-		printf("is positive \n");
+		printf(" is positive \n");
 	}
 	if (n == 0)
 	{
-		printf("is zero \n");
+		printf(" is zero \n");
 	}
-	else
+	if (n < 0)
 	{
-		printf("is negative \n");
+		printf(" is negative \n");
 	}
 	return (0);
 }
