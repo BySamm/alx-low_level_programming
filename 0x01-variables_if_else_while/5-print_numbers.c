ac66e739288a22fa096d5ca705da4cdf85c38eb9
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-17 17:55:09 +0200

    Print numbers below 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Executor
+ * Description: Print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
