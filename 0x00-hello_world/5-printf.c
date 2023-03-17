5a5fe69bd65b3a2176270bb19077acd61609a6f5
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-17 02:09:03 +0200

    Using printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-char print[] = "with proper grammar, but the outcome is a piece of art,";
-printf(print);
+:printf"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
