5a12013963d97087413a4dd0dae835be72e23346
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-22 03:36:39 +0200

    Printing all natural numbers from n.. to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Implementation
+ * Description: Printing all pocbo number from n to 98
+ * @n: Input to be checked
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
+
+
+
+
+
