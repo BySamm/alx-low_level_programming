0ea18c1b2108ecb567c8c4127dfbad90c54259e9
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-22 03:18:31 +0200

    Adding to ints a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Implementation
+ * @a: 1st int
+ * @b: 2nd int
+ * Description: Program to add two ints
+ * Return: n
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
