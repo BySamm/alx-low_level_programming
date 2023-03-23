2c991bf665ecd4a2055fc97b6fd01884f9f342d3
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-23 22:49:42 +0200

    Multiplie two ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - A function to multiplie two digits
+ * @a: First int
+ * @b: SEcond int
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
