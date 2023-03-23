41c5995b970217b7b53d3513c571fdf2ffd6ca26
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-23 10:03:06 +0200

    Checking positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - tests function that prints zero if input is zero
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
