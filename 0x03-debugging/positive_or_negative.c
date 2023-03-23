c89bc2f68293dd6e4d5a843bb093a2eb5265bd54
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-23 10:16:32 +0200

    Checking positive or negative fix_1

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Checks for positive or negative
+ * @i: Required input
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	i = 0;
+
+	if (i == 0)
+	{
+		printf("%d is zero", i);
+		printf("\n");
+	}
+}
+
