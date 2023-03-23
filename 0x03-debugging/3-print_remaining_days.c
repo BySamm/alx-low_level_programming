2c8a79e10bdb0b56f1cc2e82094345aba2735cf6
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-23 16:42:38 +0200

    Convert date to days of a year fix_2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,48 +14,49 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if ((month >= 2 && month <= 12) && (day <= 28 && day >= 1))
+		if ((month >= 1 && month <= 12) && (day >= 1 && day >= 31))
 		{
 			day++;
 		}
-		if ((month >= 6 && month <= 12) && (day <= 30 && day >= 1))
-		{
-			day++;
-		}
-		if ((month >= 11 && month <= 12) && (day <= 30 && day >= 1))
-		{
-			day++;
-		}
-		if (month == 1 && (day <= 31 && day >= 1))
-		{
-			day++;
-		}
-		if ((month >=3 && month <= 5) && (day <= 31 && day >= 1))
-		{
-			day++;
-		}
-		if ((month >= 7 && month <= 10) && (day <= 31 && day >= 1))
-		{
-			day++;
-		}
-		if (month == 12 && (day <= 31 && day >= 1))
-		{
-			day++;
-		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 1 && day == 60)
+		if (month < 1)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 		}
-		else
+		if (month > 12)
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 		}
+		if (day < 1)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		}
+		if (day > 31)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		}
+		if (month == 6 && (day >= 1 && day > 30))
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		}
+		if (month == 2 && (day >= 1 && day > 28))
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		}
+		if (month == 11 && (day >= 1 && day > 30))
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		}
+		/**
+		*else
+		*{
+		*	printf("Day of the year: %d\n", day);
+		*	printf("Remaining days: %d\n", 365 - day);
+		*}
+		*/
 	}
 }
