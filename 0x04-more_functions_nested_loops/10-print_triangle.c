2c4f7fc88947c5b38f70f65cc33c2b8db5702855
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-24 04:40:23 +0200

    Printing an inverse triange with putchar #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Functiona to print 2 trianges
+ * @size: Is the input size of triangle
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b > 0; b--)
+			{
+				putchar(' ');
+			}
+			for (b = 0; b < a; b++)
+			{
+				putchar(35);
+			}
+			if (a == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+		putchar('\n');
+	}
+}
