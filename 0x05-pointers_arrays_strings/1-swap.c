fc0b2b4e91ee09cfe10407f9723cde8a57345b4a
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-27 17:03:58 +0200

    Adding swapping horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Function to swap pointers values
+ * @a: An input
+ * @b: An input
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
