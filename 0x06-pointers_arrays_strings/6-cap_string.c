15a9fd4f771562976cc2255ee9067275bd3e1cf7
BySamm <sam.mba.byiringiro@gmail.com>
2023-03-31 01:21:04 +0200

    Capitalizing every first letter in sentence

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ * *cap_string - Function to capitilize all words
+ * @str: The container
+ * Return: the string capitalized
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int len = 0;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (i == 0)
+		{
+			str[i] = toupper(str[i]);
+		}
+		else if (str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '(' || str[i] == ')'
+				|| str[i] == '{' || str[i] == '}' || str[i] == '\n' || str[i] == '\t')
+		{
+			str[i + 1] = toupper(str[i + 1]);
+		}
+	}
+	return (str);
+}
