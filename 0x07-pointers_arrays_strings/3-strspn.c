ed59c08848c88e486dea9de79fabafeaf0640dda
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-03 23:20:04 +0200

    geting the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - Function to get prefix length
+ * @s: A string to be scanned
+ * @accept: A string containing the char to match
+ * Return: Number of chars in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n = strspn(s, accept);
+
+	return (n);
+}
+
