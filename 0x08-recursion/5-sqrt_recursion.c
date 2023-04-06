42342d1a59be6e0050b5f7f40187efa92145dd22
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-06 02:55:40 +0200

    Finding squareroot of a natural number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * sub_sqrt_recursion - A sub function to find the root
+ * @a: The guess number
+ * @sqrt: The square root
+ * Return: Results
+ */
+
+int sub_sqrt_recursion(int a, int sqrt)
+{
+	/*sqrt = a * a;*/
+
+	if (a * a == sqrt)
+	{
+		return (a);
+	}
+	if (a == sqrt)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1 * sub_sqrt_recursion(a + 1, sqrt));
+	}
+}
+
+/**
+ * _sqrt_recursion - Function to find a natural squareroot
+ * @n: The number
+ * Return: The result
+ *i
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sub_sqrt_recursion(1, n));
+	}
+}
+
