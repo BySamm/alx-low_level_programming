c688628f35248dd5aad4a44948a7f5c275a9c162
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-10 20:30:32 +0200

    Adding only positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Function to add positive numbers
+ * @argc: Counter
+ * @argv: COntainer
+ * Return: 0 or 1
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, n = 0;
+	
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isalpha(argv[i][j]))
+					{
+						printf("%s\n", "Error");
+						return (0);
+					}
+			}
+			n += atoi(argv[i]);
+		}
+		printf("%d\n", n);
+	}
+	return (0);
+}
