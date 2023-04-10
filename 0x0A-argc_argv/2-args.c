4eb09a41a9c7dfe79cfb7d3e52edf50e12b41b75
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-10 12:40:56 +0200

    Print all args passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function to be executed
+ * @argc: Counter
+ * @argv: Container
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
