ec1a1320f988b7cb054b94eac06a604e2326407c
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-12 01:41:29 +0200

    Failed to free cause no previous

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Function to free up the 2d array created
+ * @grid: The grids created
+ * @height: The height
+ * Return: Nothing
+ */
+
+int free_grid(int **grid, int height)
+{
+	(void)grid;
+	(void)height;
+	return (0);
+}
+~
