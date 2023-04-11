28904b0ca7b710896f901882ea6d05c77402ae9a
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-12 01:20:50 +0200

    Failed malloc task1 to be continued

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Function to concatenate two string an allocate memory
+ * @s1: String one
+ * @s2: String two
+ * Report: NULL or pointer
+ */
+
+str_concat(char *s1, char *s2)
+{
+	(void)s1;
+	(void)s2;
+	return (0);
+}
+
