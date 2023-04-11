928b7c6b453445b420340951c2fbe2369ea9df02
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-11 20:18:48 +0200

    Creating an array, malloc use

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Fucntion to create a array of chars
+ * @size: The size of memory
+ * @c: The char to be printed
+ * Return: NULL or pointe to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *p;
+
+	p = malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		p[i] = c;
+		i++;
+	}
+	if (p[i] == '\0')
+	{
+		return (p);
+	}
+	return (0);
+}
