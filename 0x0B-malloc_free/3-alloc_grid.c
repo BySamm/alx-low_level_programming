5db37205537889d8b0f4eef0caa2e9d1a0031867
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-12 01:31:40 +0200

    Failed grid allocation to be continued

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Function to return pointer to a 2d array
+ * @width: The width
+ * @height: The height
+ * Return: NULL or pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	(void)width;
+	(void)height;
+	return (0);
+}
