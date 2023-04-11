c9d96610423ed61780993db32559bd4c78ef296b
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-11 21:53:23 +0200

    Creating an array, malloc use fix_1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *p;
 
-	p = malloc(size * sizeof(char));
+	p = malloc(sizeof(char) * (size + 1));
 	if (size == 0)
 	{
 		return (NULL);
