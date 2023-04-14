ae86dcaca53d7dcd4a9a7adfe279560e32be424f
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-14 03:08:11 +0200

    Concatenate strings_to be cont..

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Function to concatenate two string
+ * @s1: String one
+ * @S2: String two
+ * @n: Size
+ * Return: NULL or Pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *con;
+
+	return(con);
+}
