735128d8b7c08701360456a7c044f6c2c09e64a5
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-14 03:12:49 +0200

    Allocate mem for array_to be cont...

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _calloc - Fucntion to allocate mem for an array
+ * @nmemb: Arraym
+ * @size: Size
+ * Return: NULL or Pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	return (arr);
+}
