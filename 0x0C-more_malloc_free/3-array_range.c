910ee8c0647bcfca1118b54095e12d7d011d4741
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-14 03:18:30 +0200

    Creating array of int_to be cont...

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * array_range - Function to create an array of int
+ * @min: Minimum
+ * @max: Maximum
+ * Return: NULL or Pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+
+	return (arr);
+}
