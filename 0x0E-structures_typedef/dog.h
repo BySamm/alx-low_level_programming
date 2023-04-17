8347ed4e38cee618ec7a1302b1958516f2452f72
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-17 23:48:23 +0200

    Adding first structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - Structure defined
+ * @name: Name of the puppy
+ * @age: Age of the puppy
+ * @owner: Owner of the puppy
+ * Description: The structure will be use to display the above info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
