e83536799b9b343f6f0104f23b129c7d4dea7af3
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-21 08:51:04 +0200

    Function to execute another function fix_1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; (size_t)i < size; i++)
 	{
 		action(array[i]);
