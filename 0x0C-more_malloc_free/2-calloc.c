a6638338ef9f1a896c882e26bd27e4b1781d0573
BySamm <sam.mba.byiringiro@gmail.com>
2023-04-22 21:35:51 +0200

    function tO allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,25 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * _memset - To set the first byte with the value
+ * @s: Memory to be set to
+ * @b: Char to be set
+ * @n: Num of times to set the char
+ * Return: The pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; (size_t)i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
 
 /**
  * _calloc - Fucntion to allocate mem for an array
@@ -9,7 +30,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	char *parr;
 
-	return (arr);
+	if (nmemb == 0 || size == 0)
+	{
+		return (0);
+	}
+	parr = malloc(size * nmemb);
+	if (parr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(parr, 0, nmemb * size);
+
+	return (parr);
 }
